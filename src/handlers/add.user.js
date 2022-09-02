e4247b6c968ee9ab24d10ea7d619cbe9c097e976
Dnyandeo Waghunde <dnyanu.waghunde@gmail.com>
2022-09-03 01:00:29 +0200

    update validation

--- src/handlers/add.user.js
+++ src/handlers/add.user.js
@@ -10,19 +10,34 @@ export const getInformation = async () => {
         const userEmail = document.getElementById(INPUT_EMAIL).value;
         const userPhone = document.getElementById(INPUT_PHONE).value;
 
-        const res = await fetch(urlApi, {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json',
-            },
-            body: JSON.stringify({ name: userName, email: userEmail, phone: userPhone, completed: false })
-        });
-        if (res.status == 201) {
-            const data = await res.json();
-            userComponent(data);
-        }else{
-            console.log(`Something went wrong, Status code:${res.status}`)
+        if (userName == '') {
+            alert('Empty name input field');
+        } else if (userEmail == '') {
+            alert('Empty email input field');
+        } else if (userPhone == '') {
+            alert('Empty phone input field');
+        } else {
+            const res = await fetch(urlApi, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify({ name: userName, email: userEmail, phone: userPhone, completed: false })
+            });
+            if (res.status == 201) {
+                const data = await res.json();
+                userComponent(data);
+
+                userName.value = '';
+                userEmail.value = '';
+                userPhone.value = '';
+
+            } else {
+                console.log(`Something went wrong, Status code:${res.status}`)
+            }
+
         }
+
     } catch (error) {
         console.log(error)
     }
