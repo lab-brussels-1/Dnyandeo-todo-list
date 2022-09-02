b2570dba732cbc4818168e3e2ed433db9ee9a012
Dnyandeo Waghunde <74929724+Dnyandeo33@users.noreply.github.com>
2022-09-03 01:39:48 +0200

    Update api.js

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,2 +1,2 @@
-export const urlApi = "http://localhost:5000/todos";
-//export const urlApi = "https://my-json-server.typicode.com/lab-brussels-1/Dnyandeo-todo-list/todos"
+//export const urlApi = "http://localhost:5000/todos";
+export const urlApi = "https://my-json-server.typicode.com/lab-brussels-1/Dnyandeo-todo-list/todos"
